5314299e8a85d4346315fe94f206dc5e7b89347a
Pongsiwat Wongchongchaiharn <79736225+REWERK@users.noreply.github.com>
2024-04-13 22:13:21 +0700

    notificationmenu updated

--- src/NotificationMenu.tsx
+++ src/NotificationMenu.tsx
@@ -28,7 +28,7 @@ const NotificationMenu: React.FC<NotificationMenuProps> = ({ onClose }) => {
     <div className="bg-white p-4 rounded">
       <div className="flex items-center justify-between mb-2">
         <div className="flex items-center">
-          <span>แจ้งเตือนผู้ใช้บริการ </span><span className="ml-8">({notificationCount})</span>
+          <span>แจ้งเตือนผู้ใช้บริการ </span><span className="ml-6 w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">({notificationCount})</span>
         </div>
       </div>
       <div
@@ -37,12 +37,26 @@ const NotificationMenu: React.FC<NotificationMenuProps> = ({ onClose }) => {
       >
         <span>แจ้งเตือนผู้ใช้งานระบบ</span> <span className="ml-3">({notificationCount}) {openSubMenu[1] ? <KeyboardArrowDown /> : null}</span>
       </div>
+      {openSubMenu[1] && (
+        <div className="ml-5">
+          <div className="text-gray-500 mb-1">Content 1</div>
+          <div className="text-gray-500 mb-1">Content 2</div>
+          <div className="text-gray-500">Content 3</div>
+        </div>
+      )}
       <div
         className={`flex items-center mb-2 cursor-pointer py-2 ${selectedItem === 2 ? 'text-blue-500 border-b border-gray-300' : ''}`}
         onClick={() => toggleSubMenu(2)}
       >
         <span>แจ้งเตือน refer</span> <span className="ml-14">&nbsp;({notificationCount}) {openSubMenu[2] ? <KeyboardArrowDown /> : null}</span>
       </div>
+      {openSubMenu[2] && (
+        <div className="ml-5">
+          <div className="text-gray-500 mb-1">Content A</div>
+          <div className="text-gray-500 mb-1">Content B</div>
+          <div className="text-gray-500">Content C</div>
+        </div>
+      )}
     </div>
   );
 };
